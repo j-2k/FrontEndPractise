6a0cff35f030b812f3dabcf45287f0fc70cc3606
Juma Al Remeithi <juma_ad@hotmail.com>
2023-07-11 14:25:25 +0400

    gpt testing

---
+++ GPTWEB/script.js
@@ -0,0 +1,23 @@
+import { config } from "dotenv"
+config()
+
+import { Configuration, OpenAIApi} from "openai";
+
+const openai = new OpenAIApi(new Configuration({
+    apiKey: process.env.API_KEY
+}))
+
+console.log(process.env.API_KEY);
+
+
+openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages:[{
+        role: "user",
+        content: "Hello ChatGPT"}],
+})
+.then( res => {
+    console.log(res.data.choices[0].message.content)
+})
+
+
