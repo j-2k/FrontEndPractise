b6ef023583ef93ff39541ac29b0b454f8dfd617b
Juma <juma_ad@hotmail.com>
2023-07-16 00:39:10 +0400

    testing context stuff

--- GPTPDF/app.py
+++ GPTPDF/app.py
@@ -5,8 +5,13 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms.openai import OpenAI
+#import openai
 from langchain.chat_models import ChatOpenAI
+from langchain.callbacks import get_openai_callback
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chains import ConversationChain
 
 #streamlit run app.py
 def main ():
@@ -46,17 +51,71 @@ def main ():
             docSearchRes = knowledgeDB.similarity_search(userQuestion)
             #st.write(docSearchRes)
 
-            openAIllm = ChatOpenAI(model_name="gpt-3.5-turbo")
+            openAIllm = ChatOpenAI(model_name="gpt-3.5-turbo", verbose=True)
+            #context & history
+            """
+            memory = ConversationBufferMemory(
+                memory_key="history",
+                return_messages=True)
+            
+            convoChain = ConversationalRetrievalChain.from_llm(
+                llm=openAIllm,
+                retriever=knowledgeDB.as_retriever(),
+                memory=memory
+            )
+            """
+
+            conversation = ConversationChain(
+                llm=openAIllm, 
+                # We set a low k=2, to only keep the last 2 interactions in memory
+                memory=ConversationBufferWindowMemory(k=2), 
+                verbose=True
+            )
+
+            
+
             chain = load_qa_chain(openAIllm, chain_type="stuff")
-            gptResponse = chain.run(input_documents=docSearchRes, question=userQuestion)
 
-            st.write(gptResponse);
+            with get_openai_callback() as cb:
+                gptResponse = chain.run(input_documents=docSearchRes, question=userQuestion)
+                print(cb)
+            
+            st.write(gptResponse)
 
-
-
-        
+            st.write("TEST >>> " + conversation.predict(input=userQuestion))
 
 
 
 if __name__ == '__main__':
+    #Choice()
     main()
+
+
+
+
+def Choice():
+    type = input("Type 1 for main 2 for context chat")
+    if(type == "1"):
+        main()
+    else:
+        ContextChat()
+        
+
+
+
+def ContextChat():
+    isRunning = True
+    messagesArr = ["Start"]
+    while(isRunning):
+        newMsg = input("Enter New Message")
+        messagesArr.append(newMsg)
+        print(messagesArr)
+
+        stop = input("type end to stop")
+        if(stop == "stop"):
+            isRunning = False
+
+        
+    
+
+
