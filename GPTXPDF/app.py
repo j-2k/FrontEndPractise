88a3ffcb37df7cfbb7288ad80cc4ac8bbe5cd8f6
Juma Al Remeithi <juma_ad@hotmail.com>
2023-07-17 09:05:28 +0400

    Testing pdf gpt app 2nd ver

--- GPTXPDF/app.py
+++ GPTXPDF/app.py
@@ -7,6 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
 
 
 def extractText(pdfs):
@@ -34,7 +35,7 @@ def getKnowledgeDB(rawChunks):
     return vectorDB
 
 def conversationChain(vectorstore):
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", verbose=True)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo")
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
@@ -47,46 +48,54 @@ def conversationChain(vectorstore):
 def handleUserinput(question):
     response = st.session_state.conversation({'question': question})
     st.session_state.chat_history = response['chat_history']
-
+    st.write(css, unsafe_allow_html=True)
+    
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write("USER" + message.content)
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
         else:
-            st.write("BOT" + message.content)
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="GPT PDF Chatter", page_icon=":100:")
-
+    st.write(css, unsafe_allow_html=True)
+    
     if "conversation" not in st.session_state:
-        st.session_state.conversation = None
+        st.session_state.conversation = ""
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
     st.header("Chat using your PDFs")
 
     userQuestion = st.text_input("Ask about your PDFs")
-    if userQuestion:
-        handleUserinput(userQuestion)
+    
+
 
     with st.sidebar:
         st.subheader("Your Documents")
         pdfDocs = st.file_uploader("Upload your PDFs here and click on process!", accept_multiple_files=True)
         
         if st.button("Process"):
+            if userQuestion:
+                #handleUserinput(userQuestion)
+                print("continue")
+            else:
+                return st.write(None)
+            
             with st.spinner("Loading..."):
                 # Extract PDF Text
                 rawText = extractText(pdfDocs)
-
                 # Extract Text Chunks
                 chunks = extractChunks(rawText)
-
                 # Create KnowledgeDB
                 db = getKnowledgeDB(chunks)
-
                 # create conversation chain
                 st.session_state.conversation = conversationChain(db)
+                handleUserinput(userQuestion)
 
 
 
